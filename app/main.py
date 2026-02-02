efe340b43f03f28ebaf6f3acafb3f9f80eacb468
Max Kaiser <max@kaiser.family>
2026-02-02 22:14:15 +0100

    added scalar docs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/shipment/{id}")
 def get_shipment(id: int):
     return {"id": id, "content": "wooden table", "status": "in transit"}
+
+
+@app.get("/scalar")
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
